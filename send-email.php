3c435efc16c3c4ca5f9f6897add97e856fb0bd0c
Anna <126900803+AnnaC-1@users.noreply.github.com>
2023-08-24 11:25:18 -0600

    Add files via upload

---
+++ send-email.php
@@ -0,0 +1,29 @@
+<?php
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+    
+    // Create email headers
+    $headers = 'From: '.$email."\r\n".
+        'Reply-To: '.$email."\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+    
+    // Compose email body
+    $body = "Name: $name\nEmail: $email\nPhone: $phone\nSubject: $subject\nMessage:\n$message";
+
+    // Send email
+    if(mail('annacapels@gmail.com', 'New message from your personal website', $body, $headers)) {
+        // Email sent successfully
+        header('Location: thank-you.html');
+        exit();
+    }
+    else {
+        // Email failed to send
+        header('Location: error.html');
+        exit();
+    }
+}
+?>
