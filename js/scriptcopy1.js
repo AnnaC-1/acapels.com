ad5c88c17822c9debf6724499383d9a65d733741
Anna <126900803+AnnaC-1@users.noreply.github.com>
2023-10-20 14:27:28 -0600

    Update scriptcopy1.js

--- js/scriptcopy1.js
+++ js/scriptcopy1.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+
   /* toggle icon navbar */
   let menuIcon = document.querySelector('#menu-icon');
   let navbar = document.querySelector('.navbar');
